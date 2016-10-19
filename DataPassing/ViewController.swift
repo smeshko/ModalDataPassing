c6a654a17de7f46129c979391328acf34f07ad3d
Ivaylo Tsonev <ivaylo.tsonev@iconmobile.com>
2016-10-19 15:44:23 +0200

    Initial Commit

---
+++ DataPassing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataPassing
+//
+//  Created by Ivaylo Tsonev on 19/10/16.
+//  Copyright © 2016 NoName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
